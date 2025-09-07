47d6db922780bc2937e0cef746017f8b77984ba2
rafhome <110781737+mhrafin@users.noreply.github.com>
2025-09-07 07:48:51 +0600

    Start Making the portfolio

---
+++ pelicanconf.py
@@ -0,0 +1,48 @@
+AUTHOR = 'Mahedi Hassan Rafin'
+SITENAME = 'Raf'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Europe/Rome'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+
+THEME = "theme"
+
+THEME_STATIC_DIR = 'static'
+
+THEME_STATIC_PATHS = ['static']
+
+INDEX_SAVE_AS = 'blog/index.html'
+
+TEMPLATE_PAGES = {
+    'portfolio.html': 'index.html',  # your custom landing page
+}
